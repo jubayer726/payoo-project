193c209e1f9c4ce0532b408970754af5f00ab861
JUBAYER <juelali482@gmail.com>
2025-08-23 11:35:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Login Button function
+document.getElementById('login').
+addEventListener('click', function(e){
+    e.preventDefault()
+    const mobileNumber = 17399873330;
+    const pin = 1234;
+
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+    const mobileNumberValueConv = parseInt(mobileNumberValue);
+
+    const pinValue = document.getElementById('pin-number').value;
+    const pinValueCon = parseInt(pinValue);
+    
+})
